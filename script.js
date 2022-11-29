477a15a74ca1187c02657db466b6621c2833fa38
Madan Mohan <heyitsmadan@gmail.com>
2022-11-29 20:39:31 +0530

    Fix confirm password turning red on empty field

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const confirm = document.querySelector("#confirmPassword")
 const pass = document.querySelector("#password")
 const unmatch = document.querySelector(".unmatch")
-let flag= false;
 
 confirm.addEventListener('input', (e) => {
-    flag=true
-    if(e.target.value != pass.value){
-        unmatch.setAttribute('id','true')
+    if(e.target.value.length>0){
+        if(e.target.value != pass.value)
+            unmatch.setAttribute('id','true')
+        else
+            unmatch.removeAttribute('id')
     }
     else
         unmatch.removeAttribute('id')
 })
 
 pass.addEventListener('input', (e) => {
-    if(flag){
-    if(e.target.value != confirm.value){
-        unmatch.setAttribute('id','true')
+    if(confirm.value.length>0){
+        if(e.target.value != confirm.value)
+            unmatch.setAttribute('id','true')
+        else
+            unmatch.removeAttribute('id')
     }
     else
         unmatch.removeAttribute('id')
-}
 })
