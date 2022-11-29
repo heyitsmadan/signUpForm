ecb772453699c50ec3b111b9b2bd04293b5a165e
Madan Mohan <heyitsmadan@gmail.com>
2022-11-29 23:16:21 +0530

    Add switch case to handle password strength

--- script.js
+++ script.js
@@ -23,3 +23,45 @@ pass.addEventListener('input', (e) => {
     else
         unmatch.removeAttribute('id')
 })
+
+const bgc = document.querySelector('.bgc')
+const text = document.querySelector('.text')
+pass.addEventListener('input', (e) => {
+    if(e.target.value){
+        a=0
+        b=0
+        c=0
+        text.removeAttribute('id')
+        if(e.target.value.length>5)
+            a=1
+        if(!e.target.value.match(/^[a-z0-9]+$/i) || (e.target.value.match(/[A-Z]/) && e.target.value.match(/[a-z]/)))   
+            b=1;
+        if(e.target.value.match(/[0-9]/))
+            c=1
+        strength=a+b+c 
+
+        switch(strength){
+            case 0:
+                bgc.removeAttribute("class")
+                bgc.classList.add('red')
+                break;
+            case 1:
+                bgc.removeAttribute("class")
+                bgc.classList.add('yellow')
+                break;
+            case 2:
+                bgc.removeAttribute("class")
+                bgc.classList.add('lime')
+                break;
+            case 3:
+                bgc.removeAttribute("class")
+                bgc.classList.add('green')
+                break;
+        }
+    }
+    else{
+        text.setAttribute('id','show')
+        bgc.classList.add('bgc')
+    }
+})
+
